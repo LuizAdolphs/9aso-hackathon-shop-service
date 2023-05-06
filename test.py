5036389c99ad39769ddc3e7e0aa9147b05594da8
WeslleyFelizardo <weslleyfelizardo1996@gmail.com>
2023-05-06 11:37:10 -0300

    Ajuste test e pipeline

--- test.py
+++ test.py
@@ -13,7 +13,3 @@ class Test(unittest.TestCase):
     def test_requisicao(self):
         # compara o status da requisicao (precisa ser igual a 200)
         self.assertEqual(self.result.status_code, 200)
-
-    def test_conteudo(self):
-        # verifica o retorno do conteudo da pagina
-        self.assertEqual(self.result.data.decode('utf-8'), "Laboratório Pipeline DevOps")
